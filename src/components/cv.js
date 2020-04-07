61901a07aed60cdc81b89fe21211b95b57899728
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-07 16:50:08 +0100

    try to add img

--- src/components/cv.js
+++ src/components/cv.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class Cv extends Component {
   render() {
     return(
-      <div><h1>Cv Page</h1></div>
+      <div>
+        <Grid>
+          <Cell col={4}>
+             <div style={{textAlign: 'center'}}>
+                <img
+                  src="https://mail.google.com/mail/u/0/#inbox?projector=1"
+                  alt="raf"
+                  style={{height: '200px'}}
+                />
+             </div>
+          </Cell>
+          <Cell className="cv-right-col" col={8}>Right side</Cell>
+        </Grid>
+      </div>
     )
   }
 }
