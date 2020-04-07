bf384a72276ca0e0e62d92c2001fae2b8560d50d
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-07 20:03:13 +0100

    left side of cv page done

--- src/components/cv.js
+++ src/components/cv.js
@@ -16,6 +16,20 @@ class Cv extends Component {
                   style={{height: '200px'}}
                 />
              </div>
+
+             <h2 style={{paddingTop: '2em'}}>Raffaele Castaldo</h2>
+             <h4 style={{color: 'grey'}}>Software Developer</h4>
+             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+             <p>Recently graduated in Professional Software Development from CodeClan.
+             Looking forward to applying my new tech skills in the industry.</p>
+             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+             <h5>Address</h5>
+             <p>7/4 Coxfield, Edinburgh</p>
+             <h5>Phone</h5>
+             <p>07796264489</p>
+             <h5>Email</h5>
+             <p>raffaelecastaldo881@gmail.com</p>
+             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
           </Cell>
           <Cell className="cv-right-col" col={8}>Right side</Cell>
         </Grid>
