e2741cce4557be5cb50bbc67290539a35ed10e12
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-07 12:31:00 +0100

    projects page nearly completed

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
-
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Project extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeTab: 0
+    };
+  }
+
+  toggleCategories(){
+
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+        {/* Project 1 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#000000', height: '176px', background:
+        'url(https://www.tenzione.it/wp-content/uploads/2016/09/ruby_logo.jpg_234%C3%97114_pixels.jpg) center / cover'}}>
+          Ruby Project #1</CardTitle>
+          <CardText>
+          Hello World!
+          </CardText>
+          <CardActions border>
+          <Button colored>GitHub</Button>
+          </CardActions>
+          <CardMenu style={{color: '#000000'}}>
+          <IconButton name="share" />
+          </CardMenu>
+          </Card>
+        </div>
+
+        )
+    } else if(this.state.activeTab === 1) {
+      return(
+        <div><h1>This is Java</h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return(
+        <div><h1>This is JavaScript</h1></div>
+      )
+    }
+  }
+
+
+
   render() {
     return(
-      <div><h1>Project Page</h1></div>
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+          <Tab>Ruby</Tab>
+          <Tab>Java</Tab>
+          <Tab>JavaScript</Tab>
+        </Tabs>
+
+      <section className="projects-grid">
+      <Grid>
+      <Cell col={12}>
+      <div className="content">{this.toggleCategories()}</div>
+      </Cell>
+      </Grid>
+
+      </section>
+      </div>
+
     )
   }
 }
